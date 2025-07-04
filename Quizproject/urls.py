cc3c5e5d97c860c3f72b80502916b16c9f85f3fb
Tanmay Shinde <tanmay@Tanmays-MacBook-Pro.local>
2025-07-04 22:58:10 +0530

    Debug 2

--- Quizproject/urls.py
+++ Quizproject/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from Quizproject.Quizproject import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),  # this line is important!
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
