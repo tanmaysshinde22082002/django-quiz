51f625d13479a8b459eeae6871dbeb2ca99fcf55
Tanmay Shinde <tanmay@Tanmays-MacBook-Pro.local>
2025-07-04 23:04:09 +0530

    Debug 3

--- Quizproject/urls.py
+++ Quizproject/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
-from Quizproject.Quizproject import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
