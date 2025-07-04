768ff646133507194fbdeae3aef0b38ee287ecd6
Tanmay Shinde <tanmay@Tanmays-MacBook-Pro.local>
2025-07-04 20:20:17 +0530

    Fix CSRF settings for Railway HTTPS

--- Quizproject/settings.py
+++ Quizproject/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
