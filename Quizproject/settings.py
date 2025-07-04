4dc3884bd26dcf1cf365b124535e0e03f14fd220
Tanmay Shinde <tanmay@Tanmays-MacBook-Pro.local>
2025-07-04 22:53:35 +0530

    Debug

--- Quizproject/settings.py
+++ Quizproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-dev-secret")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
